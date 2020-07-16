f793b6e33e2c511e9fca9618a381e9258bfc1f56
Thiago da Silva Martins <thiagod.smartins@icloud.com>
2020-07-16 16:34:11 -0400

    Configurações iniciais + Inicialização do renderer delegate

--- AR Drawing/ViewController.swift
+++ AR Drawing/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()//detecta orientação e posição do dispositivo
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        self.sceneView.session.run(configuration)
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.showsStatistics = true
+        self.sceneView.delegate = self
         // Do any additional setup after loading the view.
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
+        <#code#>
     }
 
 
