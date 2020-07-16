157e1190fca46af9498d9c288c158f3935dbb80c
Thiago da Silva Martins <thiagod.smartins@icloud.com>
2020-07-16 14:06:33 -0400

    Initial Commit

---
+++ AR Drawing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AR Drawing
+//
+//  Created by Thiago Martins on 16/07/20.
+//  Copyright © 2020 ThiagoMartins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
